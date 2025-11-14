8c3498f1fc9f99041fff7549ce5fe0526302ae17
supanai-h <supanai.h@gmail.com>
2025-11-15 00:01:14 +0700

    feat: create bottom navigation bar, create app color and install lucide icon

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const primary = Color(0xFFFFBC07);
+
+  // Background
+  static const backgroundPrimary = Color(0xFF282C35);
+  static const backgroundSecondary = Color(0xFF1A1C22);
+
+  // Text
+  static const textPrimary = Color(0xFFFFFFFF);
+  static const textSecondary = Color(0xFF9E9E9E);
+
+  // Icon
+  static const iconPrimary = Color(0xFF9E9E9E);
+
+  // Input
+  static const inputPrimary = Color(0xFF1A1C22);
+  static const inputText = Color(0xFFFFFFFF);
+  static const inputPlaceholder = Color(0xFF9E9E9E);
+
+  // Border
+  static const borderPrimary = Color(0xFF1A1C22);
+  static const borderSecondary = Color(0xFF282C35);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import './core/theme/app_colors.dart';
 import './routes/app_router.dart';
 import './routes/app_routes.dart';
+import './presentation/pages/home/home_page.dart';
+import './presentation/pages/trips/trip_page.dart';
+import './presentation/pages/messages/message_page.dart';
+import './presentation/pages/profile/profile_page.dart';
 
 void main() {
-  runApp(const Main());
+  runApp(const MyApp());
 }
 
-class Main extends StatelessWidget {
-  const Main({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.home,
       onGenerateRoute: AppRouter.generateRoute,
+      home: MainLayout()
+    );
+  }
+}
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    TripPage(),
+    MessagePage(),
+    ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Navigator(
+        key: GlobalKey<NavigatorState>(),
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(
+            builder: (_) => _pages[_selectedIndex],
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() => _selectedIndex = index);
+        },
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.white,
+        backgroundColor: AppColors.backgroundPrimary,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(LucideIcons.house), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(LucideIcons.scrollText), label: "Trips"),
+          BottomNavigationBarItem(icon: Icon(LucideIcons.messageCircle), label: "Messages"),
+          BottomNavigationBarItem(icon: Icon(LucideIcons.user), label: "Profile"),
+        ],
+      ),
     );
   }
 }
