8301810ae7014d84d0f92ee71ff0181ddb5ff2cf
supanai-h <supanai.h@gmail.com>
2025-11-15 02:08:25 +0700

    refactor: update UI auth/register_page

--- lib/presentation/pages/auth/register_page.dart
+++ lib/presentation/pages/auth/register_page.dart
@@ -1,13 +1,197 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:flutter_car_booking/core/theme/app_colors.dart';
+import 'package:flutter_car_booking/presentation/widgets/app_button.dart';
+import 'package:flutter_car_booking/presentation/widgets/app_text_button.dart';
+import 'package:flutter_car_booking/presentation/widgets/app_textfield.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final usernameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    final confirmPasswordController = TextEditingController();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('RegisterPage')),
-      body: const Center(child: Text('This is a StatelessWidget')),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(LucideIcons.chevronLeft, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: AppColors.backgroundPrimary,
+      ),
+      backgroundColor: AppColors.backgroundPrimary,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Create New Account',
+                      style: TextStyle(
+                        color: AppColors.textPrimary,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Set up your username and password. \n You can always change it later.',
+                      style: TextStyle(color: AppColors.textSecondary),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 24),
+              AppTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                prefixIcon: Icon(
+                  LucideIcons.user,
+                  color: AppColors.iconPrimary,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Username cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              AppTextField(
+                controller: emailController,
+                hintText: 'Email',
+                keyboardType: TextInputType.emailAddress,
+                prefixIcon: Icon(
+                  LucideIcons.mail,
+                  color: AppColors.iconPrimary,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              AppTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+                prefixIcon: Icon(
+                  LucideIcons.lockKeyhole,
+                  color: AppColors.iconPrimary,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Password cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              AppTextField(
+                controller: confirmPasswordController,
+                hintText: 'Confirm Password',
+                obscureText: true,
+                prefixIcon: Icon(
+                  LucideIcons.lockKeyhole,
+                  color: AppColors.iconPrimary,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Confirm Password cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 24),
+              AppButton(
+                text: 'Sign Up',
+                onPressed: () {},
+                type: ButtonType.primary,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: TextStyle(
+                      color: AppColors.textPrimary,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  AppTextButton(
+                    text: 'Log in',
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 24),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: AppColors.borderSecondary,
+                      thickness: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'Or sign in with',
+                      style: TextStyle(
+                        color: AppColors.textPrimary,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: AppColors.borderSecondary,
+                      thickness: 1,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: AppButton(
+                      text: 'Google',
+                      onPressed: () {},
+                      type: ButtonType.google,
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: AppButton(
+                      text: 'Facebook',
+                      onPressed: () {},
+                      type: ButtonType.facebook,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
