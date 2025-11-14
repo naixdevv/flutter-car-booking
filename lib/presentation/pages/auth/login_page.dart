c3c073d6272fab95e8e3d7a0e9f776fc8d346c7e
supanai-h <supanai.h@gmail.com>
2025-11-15 02:08:13 +0700

    refactor: update UI auth/login_page

--- lib/presentation/pages/auth/login_page.dart
+++ lib/presentation/pages/auth/login_page.dart
@@ -1,13 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:flutter_car_booking/routes/app_routes.dart';
+import 'package:flutter_car_booking/core/theme/app_colors.dart';
+import 'package:flutter_car_booking/presentation/widgets/app_button.dart';
+import 'package:flutter_car_booking/presentation/widgets/app_text_button.dart';
+import 'package:flutter_car_booking/presentation/widgets/app_textfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('LoginPage')),
-      body: const Center(child: Text('This is a StatelessWidget')),
+      backgroundColor: AppColors.backgroundPrimary,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              SizedBox(height: 24),
+              Image.asset(
+                'assets/images/logo.png',
+                width: 150,
+                height: 150,
+                fit: BoxFit.contain,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Welcome Back',
+                      style: TextStyle(
+                        color: AppColors.textPrimary,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Login to your account using email or social networks',
+                      style: TextStyle(color: AppColors.textSecondary),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 24),
+              AppTextField(
+                controller: emailController,
+                hintText: 'Email',
+                keyboardType: TextInputType.emailAddress,
+                prefixIcon: Icon(
+                  LucideIcons.mail,
+                  color: AppColors.iconPrimary,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              AppTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+                prefixIcon: Icon(
+                  LucideIcons.lockKeyhole,
+                  color: AppColors.iconPrimary,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Password cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  AppTextButton(text: 'Forgot Password?', onPressed: () {}),
+                ],
+              ),
+              SizedBox(height: 16),
+              AppButton(
+                text: 'Log In',
+                onPressed: () {},
+                type: ButtonType.primary,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'First time here?',
+                    style: TextStyle(
+                      color: AppColors.textPrimary,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  AppTextButton(
+                    text: 'Sign up',
+                    onPressed: () {
+                      Navigator.pushNamed(context, AppRoutes.register);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 24),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: AppColors.borderSecondary,
+                      thickness: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'Or sign in with',
+                      style: TextStyle(
+                        color: AppColors.textPrimary,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: AppColors.borderSecondary,
+                      thickness: 1,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: AppButton(
+                      text: 'Google',
+                      onPressed: () {},
+                      type: ButtonType.google,
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: AppButton(
+                      text: 'Facebook',
+                      onPressed: () {},
+                      type: ButtonType.facebook,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
