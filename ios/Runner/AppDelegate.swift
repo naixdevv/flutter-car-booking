59e1a9a7b4b65b1176a4be712ad7760ebaca13c8
supanai-h <supanai.h@gmail.com>
2025-11-18 03:11:14 +0700

    chore: update config ios, android google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyAJCo_6cUwOGgcCC8k46BxHkDcoW5zYCVY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
