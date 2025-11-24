95d8076130c62ba52a819f54483e4fb78100e4d7
supanai-h <supanai.h@gmail.com>
2025-11-25 00:17:13 +0700

    refactor: update date_utils formatDate from locale

--- lib/core/utils/date_utils.dart
+++ lib/core/utils/date_utils.dart
@@ -4,4 +4,11 @@ class AppDateUtils {
   static String shortDate(DateTime date) {
     return DateFormat('MMM d').format(date);
   }
+
+  static String formatDate(DateTime date, String locale) {
+    Intl.defaultLocale = locale;
+
+    final formatter = DateFormat.yMMMMEEEEd(locale);
+    return formatter.format(date);
+  }
 }
