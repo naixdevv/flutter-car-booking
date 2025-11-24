a4403f7886e8c7334752d004ef68b90ced9e2f49
supanai-h <supanai.h@gmail.com>
2025-11-24 23:41:36 +0700

    refactor: add formatters check isValidEmail input form

--- lib/core/utils/formatters.dart
+++ lib/core/utils/formatters.dart
@@ -4,4 +4,8 @@ class AppFormatters {
   static String time(DateTime date) {
     return DateFormat('HH:mm').format(date);
   }
+
+  static bool isValidEmail(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
 }
